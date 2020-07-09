d29d6efd42d7bccb847208c6fbc469604db4cfeb
Chhaya Pathak <chhaya@Atuls-MacBook-Pro.local>
2020-07-09 15:42:55 +0530

    adding all

---
+++ src/main/java/exercise/Test.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+	Long s =null;
+	List<Long> l = new ArrayList<Long>();
+	l.add(0,Long.valueOf(1));
+	System.out.println(l);
+}
+
+}
+ 
